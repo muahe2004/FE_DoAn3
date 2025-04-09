00f5257f9838beeb8f1b7f745a32dd8abd0e2cbc
Ly Van Minh <lyvanminh280504@gmail.com>
2025-04-09 20:00:09 +0700

    Update UI payment-done and call api payment

--- app/routes/payment-done.tsx
+++ app/routes/payment-done.tsx
@@ -1,17 +1,31 @@
-import { Link } from "react-router-dom";
-import Header from "~/components/Header";
-import Footer from "~/components/Footer";
-import Navbar from "~/components/Navbar";
+import Button from "~/components/Button";
+
+import "../styles/payment-done.css";
 
 
 export default function PaymentDone() {
   return (
-    <div>
-      <Header title="Nạp tiền"></Header>
-      <Navbar></Navbar>
-      <Footer></Footer>
-      
+    <div className="payment-done__wrapper">
+      {/* <Header title="Nạp tiền"></Header>       */}
+      <div className="payment-done__inner">
+        <div className="payment-done__box">
+          <img className="payment-done__icon" src="./icons/Check-white.svg" alt="" />
+        </div>
+
+        <h2 className="payment-done__thanks">Cảm ơn bạn đã thanh toán</h2>
+
+        <p className="payment-done__desc payment-done__desc--bold">Thực hiện thanh toán thành công!</p>
+
+        <p className="payment-done__desc">Đăng ký khóa học tại MLearning ngay bây giờ!</p>
+
+        <div className="payment-done__action">
+          <Button className="payment-done__btn" to="/">Khóa học</Button>
+          <Button className="payment-done__btn" to="/">Trang chủ</Button>
+        </div>
+      </div>
+
+      {/* <Footer className = 'payment-done__footer'></Footer> */}
     </div>
-    
   );
 }
+
--- app/routes/payment.tsx
+++ app/routes/payment.tsx
@@ -12,7 +12,6 @@ export default function Payment() {
   const handleBlur = (event: React.FocusEvent<HTMLInputElement | HTMLSelectElement>) => {
     const input = event.target;
     const formErr = input.parentElement?.querySelector(".payment-form__text") as HTMLElement | null;
-
     const isValid = input.value.trim() !== "";
 
     if (formErr) {
@@ -22,11 +21,53 @@ export default function Payment() {
     input.style.borderColor = isValid ? "#fff" : "red"; 
   }
 
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+
+    const moneyInput = form.querySelector<HTMLInputElement>('input[name="money"]');
+    const formErr = moneyInput?.parentElement?.querySelector(".payment-form__text") as HTMLElement | null;
+
+    const moenyValue = moneyInput?.value.trim()
+
+    if (!moenyValue || parseInt(moenyValue) === 0) {
+      if (formErr) formErr.style.opacity = "1";
+      if (moneyInput) moneyInput.style.borderColor = "red";
+      return;
+    }
+
+    const payLoad = {
+      amount: moenyValue,
+      orderInfo: "Nạp tiền tại MLearning với Momo"
+    }
+
+    try {
+      const res = await fetch('http://localhost:1000/payment', {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payLoad),
+        credentials: 'include'  // Bắt buộc để gửi cookie
+      });
+
+      const data = await res.json();
+
+      console.log(data);
+
+      if (data) {
+        window.location.href = data.payUrl;
+      } else {
+        console.error("Không nhận được đường dẫn thanh toán.");
+      }
+    } catch (error) {
+      console.error("Lỗi khi gửi yêu cầu thanh toán:", error);
+    }
+  }
+
   return (
     <div className="payment-wrapper">
       <Header title="Nạp tiền"></Header>      
       <div className="payment-inner">
-        <form action="" className="payment-form">
+        <form onSubmit={handleSubmit} action="" className="payment-form">
           <div className="payment-form__inner">
             <div className="payment-form__group">
               <label htmlFor="money" className="payment-form__label">Nhập số tiền cần nạp</label>
@@ -43,6 +84,5 @@ export default function Payment() {
 
       <Footer className = 'payment-footer'></Footer>
     </div>
-    
   );
 }
